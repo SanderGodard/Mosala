d7af29b25053158e43332c5d461f17b68cfd9ab8
Sander Godard <sander@godard.no>
2018-12-22 22:17:58 +0100

    New design Glemt Passord
    I added a new design, and commented out the old idea to keep it close in case we add a "SMS" function as well.

--- Pages/Forgot.php
+++ Pages/Forgot.php
@@ -52,20 +52,20 @@
                   //
 
     //$telefon = $_REQUEST['telefon'];
-    $gettelefon = '90189461';
+    //$gettelefon = '90189461';
 
-    $telefon0 = strlen($gettelefon) - 2;
-    $telefon1 = substr( $gettelefon , -2, 2);
-    $telefon2 = '';
+    //$telefon0 = strlen($gettelefon) - 2;
+    //$telefon1 = substr( $gettelefon , -2, 2);
+    //$telefon2 = '';
 
 
-    $counter = 0;
-    while ($counter < $telefon0) {
-      $telefon2 = $telefon2 . '*';
-      $counter++;
-    }
+    //$counter = 0;
+    //while ($counter < $telefon0) {
+    //  $telefon2 = $telefon2 . '*';
+    //  $counter++;
+    //}
 
-    $telefon = $telefon2 . $telefon1;
+    //$telefon = $telefon2 . $telefon1;
 
   ?>
 
@@ -74,19 +74,18 @@
 		<h1>Registrert</h1>
 		<div id="Hvordan"  class="BigContent" style="text-align: center;">
       <div class="GlemtDiv">
-      <h2>Glemt passord?</h2>
+        <h2>Glemt passord?</h2>
         <table>
           <tr>
             <td>Vi kan sende mail til E-post adressen din: </td>
-            <td class="SendTo"><span title="Viser ikke nøyaktig antall *"> <?php global $email; echo $email; ?></span></td>
           </tr>
           <tr>
-            <td>Eller sende en SMS til: </td>
-            <td class="SendTo"> <?php global $telefon; echo $telefon; ?></td>
+            <td class="SendTo">
+              <input title="Viser ikke nøyaktig antall *" value="<?php global $email; echo $email; ?>" disabled><br>
+            </td>
           </tr>
           <tr class="GlemtBtn">
-            <td><button tabindex="1" type="button" href="mailto:<?php global $email; echo $email; ?>" >E-mail</button></td>
-            <td class="RightButton"><button tabindex="2" type="button" href="callto:<?php global $telefon; echo $telefon; ?>">Telefon</button></td>
+            <td><button tabindex="1" type="button" href="mailto:<?php global $email; echo $email; ?>" >Send kode</button></td>
           </tr>
         </table>
         <span id="Nullstill">Så kan du lett tilbakestille passordet ditt</span>
@@ -94,6 +93,22 @@
 		</div>
 	</div>
 
+  <!--
+  <table>
+  <tr>
+  <td>Vi kan sende mail til E-post adressen din: </td>
+  <td class="SendTo"><span title="Viser ikke nøyaktig antall *"> <?php global $email; echo $email; ?></span></td>
+</tr>
+<tr>
+<td>Eller sende en SMS til: </td>
+<td class="SendTo"> <?php global $telefon; echo $telefon; ?></td>
+</tr>
+<tr class="GlemtBtn">
+<td><button tabindex="1" type="button" href="mailto:<?php global $email; echo $email; ?>" >E-mail</button></td>
+<td class="RightButton"><button tabindex="2" type="button" href="callto:<?php global $telefon; echo $telefon; ?>">Telefon</button></td>
+</tr>
+</table>
+-->
 </body>
 <footer>
 	<?php
